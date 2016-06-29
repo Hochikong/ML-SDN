24ce47f6a8beddbf910b1e07044bf5e784daae46
Hochikong <michellehzg@gmail.com>
2016-06-29 18:50:38 +0800

    modify restapi

--- TEST1/api/restapi.py
+++ TEST1/api/restapi.py
@@ -1,6 +1,7 @@
-uthor__ = 'hochikong'
+__author__ = 'hochikong'
 from flask import Flask,request
 from flask.ext.restful import Resource,Api,reqparse
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,6 +10,7 @@ todos = {'task':'get the list of docker'}
 
 parser = reqparse.RequestParser()
 parser.add_argument('name',type=str,help='get the name')
+parser.add_argument('auth-token',type=str,help='put the token here',location='headers')
 
 
 class TodoSimple(Resource):
@@ -20,8 +22,14 @@ class TodoSimple(Resource):
             return {'error':'token error'},401
 
     def put(self,todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id:todos[todo_id]}
+        args = parser.parse_args()
+        if args['auth-token'] == 'thisismytoken':
+            todos[todo_id] = request.json.get('data')
+            return todos,201
+        else:
+            return {'status':'error'},401
+
+
 
 class GetName(Resource):
     def post(self):
@@ -32,8 +40,10 @@ class GetName(Resource):
         #name = request.json.get('name')
         return {'yourame':name}
 
-api.add_resource(TodoSimple,'/<string:todo_id>')
+
+
+api.add_resource(TodoSimple,'/todo/<string:todo_id>')
 api.add_resource(GetName,'/getname')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=9100)
+    app.run(host='0.0.0.0',port=3001,debug=True)
