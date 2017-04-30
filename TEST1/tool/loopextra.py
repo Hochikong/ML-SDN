f5c149ed1156b258b634aa43f8e2408f37c80829
Administrator <hochikong@foxmail.com>
2017-04-30 19:36:16 +0800

    fix another error

--- TEST1/tool/loopextra.py
+++ TEST1/tool/loopextra.py
@@ -17,33 +17,31 @@ payload_keys = ['application/json', 'SOAPAction', '<methodCall>']
 propertys = {}
 for i in lines:
     propertys[i + 1] = []
-    propertys[i + 1].append(raw_pkt[i].src)
-    if IP in raw_pkt[i]:
+    if TCP in raw_pkt[i]:
+        propertys[i + 1].append(raw_pkt[i].src)
         propertys[i + 1].append(raw_pkt[i][IP].src)
         propertys[i + 1].append(raw_pkt[i].dst)
         propertys[i + 1].append(raw_pkt[i][IP].dst)
         propertys[i + 1].append(str(raw_pkt[i].dport))
-    if IPv6 in raw_pkt[i]:
-        propertys[i + 1].append(raw_pkt[i][IPv6].src)
-        propertys[i + 1].append(raw_pkt[i].dst)
-        propertys[i + 1].append(raw_pkt[i][IPv6].dst)
-        propertys[i + 1].append('unknown')
-    if Raw not in raw_pkt[i]:
-        propertys[i + 1].append('unknown')
-        propertys[i + 1].append('unknown')
-    if Raw in raw_pkt[i]:
-        tmp = str(raw_pkt[i][Raw].load)
-        for x in proto_keys:
-            if x in tmp:
-                propertys[i + 1].append(x)
-                break
-        if len(propertys) == 5:
+        if Raw not in raw_pkt[i]:
             propertys[i + 1].append('unknown')
-        for x in payload_keys:
-            if x in tmp:
-                propertys[i + 1].append(x)
-                break
-        if len(propertys) == 6:
+            propertys[i + 1].append('unknown')
+        if Raw in raw_pkt[i]:
+            tmp = str(raw_pkt[i][Raw].load)
+            for x in proto_keys:
+                if x in tmp:
+                    propertys[i + 1].append(x)
+                    break
+                if len(propertys) == 5:
+                    propertys[i + 1].append('unknown')
+            for x in payload_keys:
+                if x in tmp:
+                    propertys[i + 1].append(x)
+                    break
+            if len(propertys) == 6:
+                propertys[i + 1].append('unknown')
+    else:
+        for x in range(6):
             propertys[i + 1].append('unknown')
 
 
