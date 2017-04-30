234423a9bc6b2121f2b78e90dfb498b76971553f
Administrator <hochikong@foxmail.com>
2017-04-30 18:46:59 +0800

    update again

---
+++ TEST1/tool/SVM.py
@@ -0,0 +1,40 @@
+from sklearn import svm
+import time
+import shelve
+
+sample = raw_input('Enter the sample file: ')
+expdata = raw_input('Enter the experience data: ')
+
+digt_sample = shelve.open(sample)
+digt_expdata = shelve.open(expdata)
+
+sample_data = digt_sample['res']['sample']
+sample_label = digt_sample['res']['label']
+
+start = time.clock()
+
+clf = svm.SVC()
+clf.fit(sample_data, sample_label)
+
+SOAP = 0
+REST = 0
+XMLRPC = 0
+MISS = 0
+
+for i in digt_expdata['res']:
+    if clf.predict(digt_expdata['res'][i])[0] == 'SOAPAPI':
+        SOAP += 1
+    if clf.predict(digt_expdata['res'][i])[0] == 'XMLRPC':
+        XMLRPC += 1
+    if clf.predict(digt_expdata['res'][i])[0] == 'RESTAPI':
+        REST += 1
+    else:
+        MISS += 1
+
+end = time.clock()
+
+print("SOAP flows: " + SOAP)
+print("REST flows: " + REST)
+print("XMLRPC flows: " + XMLRPC)
+print("Cannot identify: " + MISS)
+print("Cost: %f seconds") % (end - start)
